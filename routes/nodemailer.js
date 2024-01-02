4d28f024809e1ed5668fdfc513c9a20f38f29860
Ariel Abad <arielabad@Ariels-MacBook-Pro.local>
2024-01-01 21:34:11 -0600

    send email using data from frontend

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -21,20 +21,20 @@ transporter.verify((err, success) => {
 });
 
 router.post("/send", function (req, res) {
-  const mailOptions = {
-    from: process.env.EMAIL,
-    to: process.env.EMAIL,
-    subject: "firejourneyapp",
-    text: "New access to demo dashboard",
-  };
+  // const mailOptions = {
+  //   from: process.env.EMAIL,
+  //   to: process.env.EMAIL,
+  //   subject: "firejourneyapp",
+  //   text: "New access to demo dashboard",
+  // };
 
-  //// Replace code above when getting data from frontend
-  //   const mailOptions = {
-  //     from: `${req.body.mailerState.email}`,
-  //     to: process.env.EMAIL,
-  //     subject: `Message from: ${req.body.mailerState.email}`,
-  //     text: `${req.body.mailerState.message}`,
-  //   };
+  // Replace code above when getting data from frontend
+    const mailOptions = {
+      from: process.env.EMAIL, //`${req.body.mailerState.email}`,
+      to: process.env.EMAIL,
+      subject: `${req.body.mailerState.subject}`,
+      text: `${req.body.mailerState.message}`,
+    };
 
   transporter.sendMail(mailOptions, function (err, data) {
     if (err) {
